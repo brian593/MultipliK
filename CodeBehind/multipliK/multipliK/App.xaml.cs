a99ede1f3177f4bfae3adb35067760fe42334d29
brianmolinabastidas <brian593@live.com>
2023-07-07 13:38:15 -0500

    .

---
+++ CodeBehind/multipliK/multipliK/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace multipliK;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ CodeBehind/multipliK/multipliK/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿namespace multipliK;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    void MultiplicaBtn_Clicked(System.Object sender, System.EventArgs e)
+    {
+		string pasos = "";
+		int numero1 = int.Parse(number1.Text);
+		pasos += $"	 {numero1}\n";
+        int numero2 = int.Parse(number2.Text);
+        pasos += $"x {numero2}\n";
+		pasos += "______________\n";
+
+		
+		char[] digitos = number2.Text.ToCharArray();
+
+		foreach (var numero in digitos.Reverse())
+		{
+			pasos += $"{(int.Parse(numero.ToString()) * numero1)}\n";
+        }
+        pasos += "______________\n";
+
+        int resultado = numero1 * numero2;
+		pasos += $"{resultado}";
+		result.Text = pasos;
+
+    }
+
+}
+
+
